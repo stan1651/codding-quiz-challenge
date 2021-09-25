48040864500f9fdc9edf2d46ab0babb94165e451
Justin Brewer <stan1651@gmail.com>
2021-09-24 19:12:15 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,28 @@
+
+var win = document.querySelector(".win");
+var lose = document.querySelector(".lose");
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+
+var chosenWord = "";
+var numBlanks = 0;
+var winCounter = 0;
+var loseCounter = 0;
+var isWin = false;
+var timer;
+var timerCount;
+
+// Arrays used to create blanks and letters on screen
+var lettersInChosenWord = [];
+var blanksLetters = [];
+
+// Array of words the user will guess
+var words = [
+  "variable",
+  "array",
+  "modulus",
+  "object",
+  "function",
+  "string",
+  "boolean",
+];
